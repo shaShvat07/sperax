44c0878be2c54a0a4b1f1c7220659d0fb0ef4f9e
Shashvat Patel <shashvatpatel2803@gmail.com>
2024-08-17 10:11:14 +0530

    Integrated Metamask wallet connection

---
+++ src/components/Wallet/Wallet.jsx
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+
+function Wallet() {
+	const [account, setAccount] = useState(null);
+	const [balance, setBalance] = useState(null);
+	const [provider, setProvider] = useState(null);
+	const [network, setNetwork] = useState(null);
+
+	useEffect(() => {
+		if (window.ethereum) {
+			initializeProvider();
+			window.ethereum.on('accountsChanged', handleAccountsChanged);
+			window.ethereum.on('chainChanged', handleChainChanged);
+		}
+
+		return () => {
+			if (window.ethereum) {
+				window.ethereum.removeListener('accountsChanged', handleAccountsChanged);
+				window.ethereum.removeListener('chainChanged', handleChainChanged);
+			}
+		};
+	}, []);
+
+	useEffect(() => {
+		if (provider) {
+			connectWallet();
+		}
+	}, [provider]);
+
+	const initializeProvider = () => {
+		const newProvider = new ethers.providers.Web3Provider(window.ethereum);
+		setProvider(newProvider);
+	};
+
+	const connectWallet = async () => {
+		if (provider) {
+			try {
+				await provider.send("eth_requestAccounts", []);
+				const signer = provider.getSigner();
+				const address = await signer.getAddress();
+				setAccount(address);
+				await refreshBalance(address);
+				await getNetwork();
+			} catch (error) {
+				console.error("Failed to connect wallet:", error);
+			}
+		} else {
+			console.log("Provider not initialized.");
+		}
+	};
+
+	const disconnectWallet = () => {
+		setAccount(null);
+		setBalance(null);
+		setNetwork(null);
+		setProvider(null);
+	};
+
+	const refreshBalance = async (address) => {
+		if (provider) {
+			const balance = await provider.getBalance(address);
+			setBalance(ethers.utils.formatEther(balance));
+		}
+	};
+
+	const handleAccountsChanged = async (accounts) => {
+		if (accounts.length > 0) {
+			setAccount(accounts[0]);
+			await refreshBalance(accounts[0]);
+		} else {
+			disconnectWallet();
+		}
+	};
+
+	const handleChainChanged = async () => {
+		initializeProvider();
+	};
+
+	const getNetwork = async () => {
+		if (provider) {
+			const network = await provider.getNetwork();
+			console.log(network);
+			setNetwork(network.name);
+		}
+	};
+
+	return (
+		<div>
+			{account ? (
+				<div>
+					<p>Connected account: {account}</p>
+					<p>Balance: {balance} ETH</p>
+					<p>Current network: {network}</p>
+					<div>
+						<button onClick={() => refreshBalance(account)}>Refresh Balance</button>
+					</div>
+					<div>
+						<button onClick={disconnectWallet}>Disconnect Wallet</button>
+					</div>
+				</div>
+			) : (
+				<button onClick={initializeProvider}>Connect MetaMask</button>
+			)}
+		</div>
+	);
+}
+
+export default Wallet;
